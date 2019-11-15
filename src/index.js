e26d1c377e87b1b9bdcf0ceae16ae483892b39a8
melaniesarah <melaniesarah@gmail.com>
2019-11-14 17:14:25 -0800

    hook up redux devtools

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ import Application from './components/Application';
 
 import './index.scss';
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    
+);
 
 ReactDOM.render(
     <Provider store={store}>
