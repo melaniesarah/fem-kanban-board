4bcbce780be57fbdbc15526eab66b8f9b36d679a
melaniesarah <melaniesarah@gmail.com>
2019-11-19 15:57:21 -0800

    create card adds card to lists in state

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,7 +1,20 @@
 import { lists as defaultLists } from '../normalized-state';
 
 const listReducer = (lists = defaultLists, action) => {
-    console.log(lists, action);
+    if (action.type === 'CARD_CREATE') {
+        const { listId, cardId } = action.payload;
+        const entities = { ...lists.entities };
+
+        entities[listId] = {
+            ...entities[listId],
+            cards: [...entities[listId].cards, cardId],
+        }
+
+        return {
+            ...lists,
+            entities,
+        }
+    }
     return lists;
 }
 
