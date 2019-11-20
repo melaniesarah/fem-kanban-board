aebd1d9beb5f114385795ca315af7aa113ec0115
melaniesarah <melaniesarah@gmail.com>
2019-11-19 16:40:51 -0800

    add lodash set function to clean up code

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,19 +1,13 @@
+import set from 'lodash/fp/set';
 import { lists as defaultLists } from '../normalized-state';
+
 
 const listReducer = (lists = defaultLists, action) => {
     if (action.type === 'CARD_CREATE') {
         const { listId, cardId } = action.payload;
-        const entities = { ...lists.entities };
 
-        entities[listId] = {
-            ...entities[listId],
-            cards: [...entities[listId].cards, cardId],
-        }
-
-        return {
-            ...lists,
-            entities,
-        }
+        const cards = [...lists.entities[listId].cards, cardId];
+        return set(['entities', listId, 'cards'], cards, lists);
     }
     return lists;
 }
