4c14263437f550fb42c8d4ffbf6f0b8fc4bcb8b1
melaniesarah <melaniesarah@gmail.com>
2019-11-22 11:06:38 -0800

    add lodash helpers to card and list reducers

--- src/reducers/card-reducer.js
+++ src/reducers/card-reducer.js
@@ -1,12 +1,12 @@
 import { cards as defaultCards } from '../normalized-state';
+import { addEntity } from './_utilities';
+
 
 const cardReducer = (cards = defaultCards, action) => {
     if (action.type === 'CARD_CREATE') {
         const { card, cardId } = action.payload;
-        return {
-            entities: { ...cards.entities, [cardId]: card },
-            id: [ ...cards.ids, cardId],
-        }
+
+        return addEntity(cards, card, cardId);
     }
     return cards;
 }
--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,13 +1,12 @@
-import set from 'lodash/fp/set';
 import { lists as defaultLists } from '../normalized-state';
+import { addChildToEntity } from './_utilities';
 
 
 const listReducer = (lists = defaultLists, action) => {
     if (action.type === 'CARD_CREATE') {
         const { listId, cardId } = action.payload;
 
-        const cards = [...lists.entities[listId].cards, cardId];
-        return set(['entities', listId, 'cards'], cards, lists);
+        return addChildToEntity(lists, listId, 'cards', cardId);
     }
     return lists;
 }
